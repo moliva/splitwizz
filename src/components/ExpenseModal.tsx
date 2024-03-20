11a7dfaa8c37cde1f030802a6ebff10ee14ca415
moliva <oliva.miguelh@gmail.com>
2024-03-20 17:30:00 -0300

    wip

--- src/components/ExpenseModal.tsx
+++ src/components/ExpenseModal.tsx
@@ -1,4 +1,6 @@
-import { For } from 'solid-js'
+import { For, createSignal } from 'solid-js'
+
+import "@thisbeyond/solid-select/style.css";
 
 import { DetailedGroup } from '../types'
 import { useAppContext } from '../context'
@@ -6,6 +8,7 @@ import { useAppContext } from '../context'
 import appStyles from '../App.module.css'
 import navStyles from './NavComponent.module.css'
 import editGroupStyles from './EditGroupComponent.module.css'
+import { SuidExample } from './ChipSelect';
 
 export type ExpenseProps = {
   group: DetailedGroup
@@ -28,26 +31,35 @@ export const ExpenseModal = (props: ExpenseProps) => {
   const dateString = new Date().toISOString()
   const date = dateString.substring(0, dateString.indexOf('.'))
 
+  const format = (item: any, type: "option" | "value") => (type === "option" ? item.label : item.name)
+  const [value, setValue] = createSignal(null);
+
+
   return <div class={editGroupStyles.modal}>
     <div class={editGroupStyles["modal-content"]}>
       <label class={editGroupStyles["modal-title"]}>New expense in <span class={appStyles['group-name']}>{group.name}</span></label>
       <input ref={descriptionRef} placeholder="Description"></input>
-      <select ref={payerRef}>
-        <For each={group.members.filter(m => m.status == 'joined').map(m => m.user)}>{(member) => (
-          <option value={member.id}>{member.email}</option>
-          // TODO - look for a way to add a select with images and multiselect (for the split part) - moliva - 2024/03/20
-          // <option value={member.id}>
-          //   <img
-          //     class={`${navStyles['profile-picture']} ${navStyles.tiny}`}
-          //     src={member.picture}
-          //     title={member.name}
-          //     crossOrigin="anonymous"
-          //     referrerPolicy="no-referrer"
-          //     alt="profile"
-          //   />
-          //   {member.email}</option>
-        )}</For>
-      </select>
+<SuidExample />
+
+      {
+
+        // <select ref={payerRef}>
+        //   <For each={group.members.filter(m => m.status == 'joined').map(m => m.user)}>{(member) => (
+        //     <option value={member.id}>{member.email}</option>
+        //     // TODO - look for a way to add a select with images and multiselect (for the split part) - moliva - 2024/03/20
+        //     // <option value={member.id}>
+        //     //   <img
+        //     //     class={`${navStyles['profile-picture']} ${navStyles.tiny}`}
+        //     //     src={member.picture}
+        //     //     title={member.name}
+        //     //     crossOrigin="anonymous"
+        //     //     referrerPolicy="no-referrer"
+        //     //     alt="profile"
+        //     //   />
+        //     //   {member.email}</option>
+        //   )}</For>
+        // </select>
+      }
       <div style={{ display: 'inline-flex' }}>
         <select ref={currencyRef}>
           <For each={state().currencies}>{(currency) => (
