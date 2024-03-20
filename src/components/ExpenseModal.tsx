b9595261771e7483e1c089d5a091cf7cf1328fc1
moliva <oliva.miguelh@gmail.com>
2024-03-20 19:41:26 -0300

    feat: select payer with custom option components

--- src/components/ExpenseModal.tsx
+++ src/components/ExpenseModal.tsx
@@ -1,9 +1,10 @@
 import { For } from 'solid-js'
 import { MultiSelect } from '@digichanges/solid-multiselect'
 
-import { DetailedGroup } from '../types'
+import { User, DetailedGroup } from '../types'
 import { useAppContext } from '../context'
 
+import styles from './ExpenseModal.module.css'
 import appStyles from '../App.module.css'
 import navStyles from './NavComponent.module.css'
 import editGroupStyles from './EditGroupComponent.module.css'
@@ -32,6 +33,7 @@ export const ExpenseModal = (props: ExpenseProps) => {
   const users = members.map(m => m.user)
   const me = members.filter(m => m.user.email === state().identity?.identity.email).map(m => m.user)
 
+
   return <div class={editGroupStyles.modal}>
     <div class={editGroupStyles["modal-content"]}>
       <label class={editGroupStyles["modal-title"]}>New expense in <span class={appStyles['group-name']}>{group.name}</span></label>
@@ -40,12 +42,25 @@ export const ExpenseModal = (props: ExpenseProps) => {
         options={users}
         isObject
         displayValue="email"
+        renderValue={(member: User) => <div class={styles['select-user-option']}>
+          <img
+            class={`${navStyles['profile-picture']} ${styles.tiny}`}
+            src={member.picture}
+            title={member.email}
+            crossOrigin="anonymous"
+            referrerPolicy="no-referrer"
+            alt="profile"
+          />
+          <span>{member.name}</span>
+        </div>}
         selectedValues={me}
         selectionLimit={1}
         hidePlaceholder={true}
         placeholder="Who's paying"
+        closeOnSelect={true}
         style={{
-          optionContainer: { 'background-color': '#282c34' }
+          optionContainer: { 'background-color': '#282c34' },
+          option: { display: 'flex', 'align-items': 'center', 'height': '40px', margin: '0', padding: '0 10px' }
         }}
       />
       <div style={{ display: 'inline-flex' }}>
