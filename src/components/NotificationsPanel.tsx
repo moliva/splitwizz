3907bbcb25331c48313728bb18829162cbf0bedc
moliva <oliva.miguelh@gmail.com>
2024-03-26 15:41:51 -0300

    styles: improving responsiveness

--- src/components/NotificationsPanel.tsx
+++ src/components/NotificationsPanel.tsx
@@ -1,11 +1,12 @@
-import { For, createSignal, Resource } from 'solid-js'
+import { For, createSignal, Resource, Match, Switch } from 'solid-js'
+import { A } from '@solidjs/router'
 
 import { CurrencyId, Notification, NotificationAction } from '../types'
 import { useAppContext } from '../context'
 
 import styles from '../App.module.css'
+import groupStyles from '../pages/Group.module.css'
 import editGroupstyles from './EditGroupComponent.module.css'
-import { A } from '@solidjs/router'
 
 export type NotificationsProps = {
   notifications: Resource<Notification[]>
@@ -47,36 +48,41 @@ export const NotificationsPanel = (props: NotificationsProps) => {
     <div class={editGroupstyles["modal-content"]}>
       <h2>Notifications</h2>
       <div class={styles['notification-cards']}>
-        <For each={notifications()}>{(notification) =>
-          <div class={styles['notification-card']}>
-            {notification.data.kind === 'invite' ? (
-              <>
-                <label>You've been invited to group <span class={styles['group-name']}>{notification.data.group.name}</span></label>
-                <div class={styles['notification-card-controls']}>
-                  {wip()[notification.id!]
-                    ? <span style={{ 'font-style': 'oblique' }}>loading...</span>
-                    : <>
-                      <button class={`${styles['notification-button']} ${styles.primary}`} onClick={onAction('joined', notification)}>
-                        Accept
-                      </button>
-                      <button class={`${styles['notification-button']} ${styles.cancel}`} onClick={onAction('rejected', notification)}>
-                        Decline
-                      </button>
-                    </>
-                  }
-                </div>
-              </>
-            ) : (
-              <>
-                <label>{notification.data.created_by.name} registered a payment of <span style={{ color: 'green' }}>{formatPrice(notification.data.currency_id, notification.data.amount)}</span> {notification.data.payer.email === state().identity!.identity.email ? 'from' : 'to'} you in group <A href={`${import.meta.env.BASE_URL}groups/${notification.data.group.id}`} class={styles['group-name']}>{notification.data.group.name}</A></label>
-              </>
-            )}
-            <span title='Archive notification' class={styles['archive-notification']} onClick={() => archiveNotifications([notification])}>⨯</span>
-          </div>
-        }</For>
+        <Switch fallback={<div class={groupStyles['settled-up-container']}><label class={groupStyles['settled-up']} style={{ 'font-size': '20px', "margin-bottom": '15px' }}>All settled! 🪄</label></div>}>
+          <Match when={(notifications() ?? []).length > 0}>
+            <For each={notifications()}>{(notification) =>
+              <div class={styles['notification-card']}>
+                {notification.data.kind === 'invite' ? (
+                  <>
+                    <label>You've been invited to group <span class={styles['group-name']}>{notification.data.group.name}</span></label>
+                    <div class={styles['notification-card-controls']}>
+                      {wip()[notification.id!]
+                        ? <span style={{ 'font-style': 'oblique' }}>loading...</span>
+                        : <>
+                          <button class={`${styles['notification-button']} ${styles.primary}`} onClick={onAction('joined', notification)}>
+                            Accept
+                          </button>
+                          <button class={`${styles['notification-button']} ${styles.cancel}`} onClick={onAction('rejected', notification)}>
+                            Decline
+                          </button>
+                        </>
+                      }
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    <label>{notification.data.created_by.name} registered a payment of <span style={{ color: 'green' }}>{formatPrice(notification.data.currency_id, notification.data.amount)}</span> {notification.data.payer.email === state().identity!.identity.email ? 'from' : 'to'} you in group <A href={`${import.meta.env.BASE_URL}groups/${notification.data.group.id}`} class={styles['group-name']}>{notification.data.group.name}</A></label>
+                  </>
+                )}
+                <span title='Archive notification' class={styles['archive-notification']} onClick={() => archiveNotifications([notification])}>⨯</span>
+              </div>
+            }</For>
+
+          </Match>
+        </Switch>
       </div>
       <div class={editGroupstyles['modal-controls']}>
-        <button class={`${styles.button} ${styles.delete}`} onClick={onArchiveAll}>Archive all</button>
+        <button class={`${styles.button} ${styles.delete}`} onClick={onArchiveAll} disabled={(notifications() ?? []).length === 0}>Archive all</button>
         <button class={`${styles.button} ${styles.secondary}`} onClick={props.onClose}>Close</button>
       </div>
     </div>
