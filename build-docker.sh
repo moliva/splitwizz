e1c39ee48c039cda5c30f68a968a9d2c1371301b
moliva <oliva.miguelh@gmail.com>
2024-12-23 13:36:08 -0300

    chore: bump

--- build-docker.sh
+++ build-docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 dockerfile='Dockerfile'
-image_name='splitwizz:v34'
+image_name='splitwizz:v35'
 
 echo "Building $image_name"
 docker build  -f "$dockerfile" -t "$image_name" .
